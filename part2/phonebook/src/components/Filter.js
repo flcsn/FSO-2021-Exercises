da5e3142e44c7370258ed7c1e4a286981136940f
flcsn <flacson98@gmail.com>
2021-11-05 15:57:14 +0800

    Revert "completed exercises 2.6-2.10"
    This reverts commit 7e3e7e8fd95cd41c35adab5fdb8b9cecddc05cb6.

--- part2/phonebook/src/components/Filter.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const Filter = ({eventHandler}) => 
-  <div> filter shown with <input onChange={eventHandler} /> </div>
-
-export default Filter
--- part2/phonebook/src/components/Persons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Persons = ({filteredPersons}) => (
-  <div>
-    {filteredPersons.map((person) => <p key={person.id}>{person.name} {person.number}</p>)}
-  </div>
-)
-
-export default Persons
