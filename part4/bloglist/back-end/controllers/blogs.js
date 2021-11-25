d6499e21bfbb7e53acae33ebd571db4c715efe03
flcsn <flacson98@gmail.com>
2021-11-25 10:54:00 +0800

    completed exercises 4.13-4.14

--- part4/bloglist/back-end/controllers/blogs.js
+++ part4/bloglist/back-end/controllers/blogs.js
@@ -16,4 +16,21 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updatedBlog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes
+  }
+
+  const result = await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+  response.json(result)
+})
+
 module.exports = blogsRouter
