0859f1cbc75d1689a77500d0906ef51adefc489a
flcsn <flacson98@gmail.com>
2021-12-22 17:20:58 +0800

    completed exercises 7.4-7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom'
 
 import React, { useState } from 'react'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -61,24 +62,30 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const object = {
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     }
     props.addNew(object)
-    console.log(object)
+    console.log('created', object)
     history.push('/')
-    props.notify(`a new anecdote '${content}' created!`)
+    props.notify(`a new anecdote '${content.value}' created!`)
+  }
+
+  const resetAllFields = () => {
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -87,17 +94,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content.value} onChange={content.onChange} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input  name='author' value={author.value} onChange={author.onChange}/>
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input  name='info' value={info.value} onChange={info.onChange}/>
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset' onClick={resetAllFields}>reset</button>
       </form>
     </div>
   )
